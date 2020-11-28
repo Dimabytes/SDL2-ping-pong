396170c214f728a7d343c2e96a0b178e1a156555
dimabytes <dimabytes@gmail.com>
2020-11-29 00:34:48 +0300

    start menu

--- src/collision.h
+++
@@ -1,7 +0,0 @@
-#include <SDL.h>
-
-bool collisionLeftRight(SDL_Rect a, SDL_Rect b);
-
-bool collisionTopBottom(SDL_Rect a, SDL_Rect b);
-
-bool collisionDetector(SDL_Rect a, SDL_Rect b);
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,116 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
-#include "collision.h"
-#include "Dot.h"
-#include "Plank.h"
+#include "game/game.h"
 #include <SDL.h>
-#include <SDL_image.h>
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include <SDL_ttf.h>
+#include "globals.h"
 
 SDL_Window *gWindow = nullptr;
 SDL_Renderer *gRenderer = nullptr;
+
+void close() {
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+    gRenderer = nullptr;
+    SDL_Quit();
+}
+
+int showmenu(SDL_Renderer *renderer, TTF_Font *font) {
+    int x, y;
+    bool isMenu = true;
+    const int NUMMENU = 4;
+    int width = SCREEN_WIDTH, height = SCREEN_HEIGHT;
+
+    const char *labels[NUMMENU] = {"Rules", "Scores", "Change background", "Play"};
+    SDL_Surface *menus[NUMMENU];
+    bool hovered[NUMMENU] = {false, false, false, false};
+    bool selected[NUMMENU] = {false, false, false, false};
+    SDL_Color color[2] = {{255, 255, 255},
+                          {0,   0,   255}};
+    SDL_Rect pos[NUMMENU];
+
+    Game game(gRenderer);
+
+    for (int i = 0; i < NUMMENU; ++i) {
+        menus[i] = TTF_RenderUTF8_Solid(font, labels[i], color[0]);
+        pos[i].x = 30;
+        pos[i].y = 100 + i * 60;
+        pos[i].w = menus[i]->clip_rect.w;
+        pos[i].h = menus[i]->clip_rect.h;
+    }
+
+    SDL_Event event;
+    while (1) {
+        switch (event.type) {
+            case SDL_QUIT:
+                for (auto & menu : menus) {
+                    SDL_FreeSurface(menu);
+                }
+                close();
+                return 1;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    for (auto & menu : menus) {
+                        SDL_FreeSurface(menu);
+                    }
+                    close();
+                    return 1;
+                }
+        }
+        for (int i = 0; i < NUMMENU; ++i) {
+            if(selected[i] && i == 0){
+                game.handleEvent(&event);
+                isMenu = false;
+                break;
+            }
+        }
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_MOUSEMOTION:
+                    x = event.motion.x;
+                    y = event.motion.y;
+                    for (int i = 0; i < NUMMENU; i++) {
+                        if (x >= pos[i].x && x <= pos[i].x + pos[i].w &&
+                            y >= pos[i].y && y <= pos[i].y + pos[i].h) {
+                            if (!hovered[i]) {
+                                hovered[i] = true;
+                                SDL_FreeSurface(menus[i]);
+                                menus[i] = TTF_RenderText_Solid(font, labels[i], color[1]);
+                            }
+                        } else if (hovered[i]) {
+                            hovered[i] = false;
+                            SDL_FreeSurface(menus[i]);
+                            menus[i] = TTF_RenderText_Solid(font, labels[i], color[0]);
+                        }
+                    }
+                    break;
+
+                case SDL_MOUSEBUTTONDOWN:
+
+                    x = event.button.x;
+                    y = event.button.y;
+                    for (int i = 0; i < NUMMENU; i += 1) {
+                        if (x >= pos[i].x && x <= pos[i].x + pos[i].w &&
+                            y >= pos[i].y && y <= pos[i].y + pos[i].h) {
+                            selected[i] = true;
+                        }
+                    }
+                    break;
+            }
+        }
+        if(isMenu){
+            for (int i = 0; i < NUMMENU; i += 1) {
+                SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, menus[i]);
+                SDL_QueryTexture(texture, nullptr, nullptr, &width, &height);
+                SDL_Rect rectGroup = {pos[i].x, pos[i].y, width, height};
+                SDL_RenderCopy(renderer, texture, nullptr, &rectGroup);
+            }
+        }
+        SDL_RenderPresent(gRenderer);
+    }
+}
 
 bool init() {
     srand(time(nullptr) % 100);
@@ -43,60 +142,15 @@ bool init() {
     return success;
 }
 
-
-void close() {
-    SDL_DestroyRenderer(gRenderer);
-    SDL_DestroyWindow(gWindow);
-    gWindow = nullptr;
-    gRenderer = nullptr;
-    SDL_Quit();
-}
-
 int main() {
     if (!init()) {
         printf("FAILED TO INITIALIZE\n");
         return 1;
     }
-
-    Dot dot;
-    Plank plank1(20, SCREEN_HEIGHT / 2 - 35);
-    Plank plank2(SCREEN_WIDTH - 40, SCREEN_HEIGHT / 2 - 35);
-    if (!dot.init(gRenderer) || !plank1.init(gRenderer) || !plank2.init(gRenderer)) {
-        printf("MEDIA LOAD FAILED\n");
-        close();
-        return 1;
-    }
-
-    bool quit = false;
-    SDL_Event e;
-
-    while (!quit) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-            plank1.handleEvent(e, 0);
-            plank2.handleEvent(e, 1);
-        }
-
-        plank1.move(SCREEN_HEIGHT);
-        plank2.move(SCREEN_HEIGHT);
-        dot.move(plank1.plankCollider, plank2.plankCollider, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-        SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
-        SDL_RenderClear(gRenderer);
-
-        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-        for (int i = 0; i < SCREEN_HEIGHT; i += 4) {
-            SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
-        }
-
-        plank1.render(gRenderer);
-        plank2.render(gRenderer);
-        dot.render(gRenderer);
-        SDL_RenderPresent(gRenderer);
-    }
+    TTF_Init();
+    TTF_Font *font;
+    font = TTF_OpenFont("fonts/font.ttf", 25);
+    showmenu(gRenderer, font);
     close();
     return 0;
 }
